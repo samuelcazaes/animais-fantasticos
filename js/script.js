e3bfff9370171050c02ba881e2bbdac46e4f4b3e
Samuel Cazaes <samuelcazaes@gmail.com>
2023-10-09 14:09:18 -0300

    tooltip refatorada

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
+import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
@@ -29,8 +29,10 @@ const modal = new Modal(
 );
 modal.init();
 
+const toolTip = new Tooltip("[data-tooltip]");
+toolTip.init();
+
 initAnimacaoScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
